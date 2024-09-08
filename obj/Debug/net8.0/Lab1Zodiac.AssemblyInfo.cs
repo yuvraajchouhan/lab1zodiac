6696866f1b5743f254c179e6bfe77f250f88b3a4
Yuvraaj Chouhan <yuvraajchouhan.79@gmail.com>
2024-09-08 15:42:14 -0700

    second commit

--- obj/Debug/net8.0/Lab1Zodiac.AssemblyInfo.cs
+++ obj/Debug/net8.0/Lab1Zodiac.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab1Zodiac")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac3d9849c07f4e477e48bc645d4b9840cba09489")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab1Zodiac")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab1Zodiac")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
